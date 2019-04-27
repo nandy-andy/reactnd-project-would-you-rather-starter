cced19d189668db885482bfbfec2542d03b124aa
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-27 15:59:16 +0200

    display answered and unanswered quesitons

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 
 class QuestionsList extends Component {
     render() {
         return (
-            <p>Questions</p>
+            <div>
+                <h3>Unanswered</h3>
+                <ul>
+                {this.props.answeredQuestions.map((question) => (
+                    <li key={question.id}>{question.author}</li>
+                ))}
+                </ul>
+                <h3>Answered</h3>
+                <ul>
+                {this.props.unansweredQuestions.map((question) => (
+                    <li key={question.id}>{question.author}</li>
+                ))}
+                </ul>
+            </div>
         );
     }
 }
 
-export default QuestionsList;
+function mapStateToProps( { authedUser, users, questions } ) {
+    return {
+        answeredQuestions: Object.values(questions).filter(
+            (question) => Object.keys(users[authedUser].answers).includes(question.id)
+        ),
+        unansweredQuestions: Object.values(questions).filter(
+            (question) => !Object.keys(users[authedUser].answers).includes(question.id)
+        )
+    };
+}
+
+export default connect(mapStateToProps)(QuestionsList);
