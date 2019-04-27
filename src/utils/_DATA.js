3a253244f9c3d4587524d53ec261b79079e821a6
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-27 22:32:38 +0200

    update avatars

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: 'http://i.pravatar.cc/160',
+    avatarURL: '/avatars/160-sarah.jpeg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: 'http://i.pravatar.cc/161',
+    avatarURL: '/avatars/160-tyler.jpeg',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: 'http://i.pravatar.cc/162',
+    avatarURL: '/avatars/160-john.jpeg',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
