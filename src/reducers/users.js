aba91e63e04a4fe39ae506336059ea8d7ba85b11
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-28 02:17:09 +0200

    fix bug in users slice when adding new question

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -12,10 +12,7 @@ export default function users (state = {}, action) {
                 ...state,
                 [action.question.author]: {
                     ...state[action.question.author],
-                    questions: {
-                        ...state[action.question.author].questions,
-                        questions: state[action.question.author].questions.concat([action.question.id])
-                    }
+                    questions: state[action.question.author].questions.concat([action.question.id])
                 }
             };
         case SAVE_USER_ANSWER:
