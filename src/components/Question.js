0b681ef506c08055d71a3c1cef7dbcd823a69b93
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-28 23:51:47 +0200

    style questions on homepage

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 
+import Alert from 'react-bootstrap/Alert';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 import { handleQuestionAnswer } from '../actions/shared';
 
 class Question extends Component {
@@ -33,7 +38,7 @@ class Question extends Component {
 
         if (question === null) {
             return (
-                <li>The question you're looking for does not exist. Sorry.</li>
+                <Alert variant='warning'>The question you're looking for does not exist. Sorry.</Alert>
             );
         }
 
@@ -44,42 +49,49 @@ class Question extends Component {
             && !optionTwo.votes.includes(authedUser);
 
         return (
-            <li>
-                <div>
-                    {authedUser === null && (
-                        <p className='please-login'>In order to answer the question please login.</p>
-                    )}
-                    <img alt={author} src={avatarUrl} />
-                    <p>Would you rather...</p>
-                    {isHomepage && (
+            <Container as='li'>
+                {authedUser === null && (
+                    <Alert variant='info'>In order to answer the question please login.</Alert>
+                )}
+                <Row>
+                    <Col sm='auto'>
+                        <img alt={author} src={avatarUrl} />
+                    </Col>
+                    <Col sm='auto' className='question'>
+                        <p>{users[author].name} asks:</p>
+                        <p>Would you rather...</p>
+                            {isHomepage && (
+                                <div className='options'>
+                                    <ul>
+                                        <li className={this.getVoteClass(optionOne.votes, authedUser)}>
+                                            ...{optionOne.text}?
+                                        </li>
+                                        <li className={this.getVoteClass(optionTwo.votes, authedUser)}>
+                                            ...{optionTwo.text}?
+                                        </li>
+                                    </ul>
+                                    <NavLink to={`/questions/${id}`} className='btn btn-secondary'>Open the poll</NavLink>
+                                </div>
+                            )}
+                        {!isHomepage && !canVote && (
                         <ul>
-                            <li className={this.getVoteClass(optionOne.votes, authedUser)}>
-                                ...{optionOne.text}?
-                            </li>
-                            <li className={this.getVoteClass(optionTwo.votes, authedUser)}>
-                                ...{optionTwo.text}?
-                            </li>
-                            <p><NavLink to={`/questions/${id}`}>Open the poll</NavLink></p>
+                        <li className={this.getVoteClass(optionOne.votes, authedUser)}>
+                            {optionOne.text} {(optionOne.votes.length/(optionOne.votes.length + optionTwo.votes.length))*100}% ({optionOne.votes.length || 0})
+                        </li>
+                        <li className={this.getVoteClass(optionTwo.votes, authedUser)}>
+                            {optionTwo.text} {(optionTwo.votes.length/(optionOne.votes.length + optionTwo.votes.length))*100}% ({optionTwo.votes.length || 0})
+                        </li>
                         </ul>
-                    )}
-                    {!isHomepage && !canVote && (
+                        )}
+                        {!isHomepage && canVote && (
                         <ul>
-                            <li className={this.getVoteClass(optionOne.votes, authedUser)}>
-                                {optionOne.text} {(optionOne.votes.length/(optionOne.votes.length + optionTwo.votes.length))*100}% ({optionOne.votes.length || 0})
-                            </li>
-                            <li className={this.getVoteClass(optionTwo.votes, authedUser)}>
-                                {optionTwo.text} {(optionTwo.votes.length/(optionOne.votes.length + optionTwo.votes.length))*100}% ({optionTwo.votes.length || 0})
-                            </li>
+                        <li><input type='radio' id={this.getOptionId(id, 1)} name={id} onClick={(event) => this.onVote(event, id, 'optionOne')} /><label htmlFor={this.getOptionId(id, 1)}>{optionOne.text}</label></li>
+                        <li><input type='radio' id={this.getOptionId(id, 2)} name={id} onClick={(event) => this.onVote(event, id, 'optionTwo')} /><label htmlFor={this.getOptionId(id, 2)}>{optionTwo.text}</label></li>
                         </ul>
-                    )}
-                    {!isHomepage && canVote && (
-                        <ul>
-                            <li><input type='radio' id={this.getOptionId(id, 1)} name={id} onClick={(event) => this.onVote(event, id, 'optionOne')} /><label htmlFor={this.getOptionId(id, 1)}>{optionOne.text}</label></li>
-                            <li><input type='radio' id={this.getOptionId(id, 2)} name={id} onClick={(event) => this.onVote(event, id, 'optionTwo')} /><label htmlFor={this.getOptionId(id, 2)}>{optionTwo.text}</label></li>
-                        </ul>
-                    )}
-                </div>
-            </li>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
