7d0178719f97a21956c8c64a811f5ddf6c8a5cc7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2019-04-28 13:31:15 +0200

    sort questions on a list

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -26,15 +26,25 @@ class QuestionsList extends Component {
     }
 }
 
+function compareQuestions(question1, question2) {
+    if (question1.timestamp > question2.timestamp) {
+        return -1;
+    } else if ( question1.timestamp < question2.timestamp ) {
+        return 1;
+    }
+
+    return 0;
+}
+
 function mapStateToProps( { authedUser, users, questions } ) {
     return {
         authedUser,
         answeredQuestions: Object.values(questions).filter(
             (question) => Object.keys(users[authedUser].answers).includes(question.id)
-        ),
+        ).sort(compareQuestions),
         unansweredQuestions: Object.values(questions).filter(
             (question) => !Object.keys(users[authedUser].answers).includes(question.id)
-        ),
+        ).sort(compareQuestions),
         users
     };
 }
